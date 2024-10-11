99d5d3d2ec96ed30eaf9234d66f57b3e58c5a576
Ronald Suplina <tyger995@gmail.com>
2024-10-11 19:21:33 +0100

    Add Superset Files

---
+++ containers/superset/superset-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create Admin user, you can read these values from env or anywhere else possible
+superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD"
+
+# Upgrading Superset metastore
+superset db upgrade
+
+# setup roles and permissions
+superset superset init 
+
+superset set_database_uri --database_name PostgreSQL --uri "$SUPERSET_DATABASE_URI"
+
+
+# Starting server
+/bin/sh -c /usr/bin/run-server.sh
---
+++ containers/superset/superset_config.py
@@ -0,0 +1,7 @@
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+}
+
+ENABLE_PROXY_FIX = True
+SECRET_KEY = "123test"
+SQLALCHEMY_DATABASE_URI = "postgresql://superset:superset_password@postgres:5432/superset_db"
